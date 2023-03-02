b4d66661fca07f6b5d764ffbdce6291821b00efb
Derek Anderson <dmikey@users.noreply.github.com>
2023-03-02 00:24:03 +0000

    adds brace expansion to support wild card and spaces

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,58 +5,55 @@ set -eu
 
 printf "\n📦 Creating %s archive...\n" "$INPUT_TYPE"
 
-if [ "$INPUT_DIRECTORY" != "." ] 
+if [ "$INPUT_DIRECTORY" != "." ]
 then
   cd "$INPUT_DIRECTORY"
 fi
 
-if [ "$INPUT_TYPE" = "zip" ] 
+if [ "$INPUT_TYPE" = "zip" ]
 then
   if [ "$RUNNER_OS" = "Windows" ]
   then
-    if [ -z "$INPUT_EXCLUSIONS" ] 
+    if [ -z "$INPUT_EXCLUSIONS" ]
     then
-      7z a -tzip "$INPUT_FILENAME" "$INPUT_PATH" $INPUT_CUSTOM || { printf "\n⛔ Unable to create %s archive.\n" "$INPUT_TYPE"; exit 1;  }
+      7z a -tzip "$INPUT_FILENAME" "${INPUT_PATH[@]}" "${INPUT_CUSTOM[@]}" || { printf "\n⛔ Unable to create %s archive.\n" "$INPUT_TYPE"; exit 1; }
     else
-      EXCLUSIONS=''
+      EXCLUSIONS=()
 
-      for EXCLUSION in $INPUT_EXCLUSIONS
+      for EXCLUSION in "${INPUT_EXCLUSIONS[@]}"
       do
-        EXCLUSIONS+=" -x!"
-        EXCLUSIONS+=$EXCLUSION
+        EXCLUSIONS+=("-x!$EXCLUSION")
       done
 
-      for EXCLUSION in $INPUT_RECURSIVE_EXCLUSIONS
+      for EXCLUSION in "${INPUT_RECURSIVE_EXCLUSIONS[@]}"
       do
-        EXCLUSIONS+=" -xr!"
-        EXCLUSIONS+=$EXCLUSION
+        EXCLUSIONS+=("-xr!$EXCLUSION")
       done
 
-      7z a -tzip "$INPUT_FILENAME" "$INPUT_PATH" $EXCLUSIONS $INPUT_CUSTOM || { printf "\n⛔ Unable to create %s archive.\n" "$INPUT_TYPE"; exit 1;  }
+      7z a -tzip "$INPUT_FILENAME" "${INPUT_PATH[@]}" "${EXCLUSIONS[@]}" "${INPUT_CUSTOM[@]}" || { printf "\n⛔ Unable to create %s archive.\n" "$INPUT_TYPE"; exit 1; }
     fi
   else
-    if [ -z "$INPUT_EXCLUSIONS" ] 
+    if [ -z "$INPUT_EXCLUSIONS" ]
     then
-      zip -r "$INPUT_FILENAME" "$INPUT_PATH" $INPUT_CUSTOM || { printf "\n⛔ Unable to create %s archive.\n" "$INPUT_TYPE"; exit 1;  }
+      zip -r "$INPUT_FILENAME" "${INPUT_PATH[@]}" "${INPUT_CUSTOM[@]}" || { printf "\n⛔ Unable to create %s archive.\n" "$INPUT_TYPE"; exit 1; }
     else
-      zip -r "$INPUT_FILENAME" "$INPUT_PATH" -x $INPUT_EXCLUSIONS $INPUT_CUSTOM || { printf "\n⛔ Unable to create %s archive.\n" "$INPUT_TYPE"; exit 1;  }
+      zip -r "$INPUT_FILENAME" "${INPUT_PATH[@]}" -x "${INPUT_EXCLUSIONS[@]}" "${INPUT_CUSTOM[@]}" || { printf "\n⛔ Unable to create %s archive.\n" "$INPUT_TYPE"; exit 1; }
     fi
   fi
-elif [ "$INPUT_TYPE" = "tar" ] || [ "$INPUT_TYPE" = "tar.gz" ] 
+elif [ "$INPUT_TYPE" = "tar" ] || [ "$INPUT_TYPE" = "tar.gz" ]
 then
-  if [ -z "$INPUT_EXCLUSIONS" ] 
+  if [ -z "$INPUT_EXCLUSIONS" ]
   then
-    tar -zcvf "$INPUT_FILENAME" "$INPUT_PATH" $INPUT_CUSTOM || { printf "\n⛔ Unable to create %s archive.\n" "$INPUT_TYPE"; exit 1;  }
+    tar -zcvf "$INPUT_FILENAME" "${INPUT_PATH[@]}" "${INPUT_CUSTOM[@]}" || { printf "\n⛔ Unable to create %s archive.\n" "$INPUT_TYPE"; exit 1; }
   else
-    EXCLUSIONS=''
+    EXCLUSIONS=()
 
-    for EXCLUSION in $INPUT_EXCLUSIONS
+    for EXCLUSION in "${INPUT_EXCLUSIONS[@]}"
     do
-      EXCLUSIONS+=" --exclude="
-      EXCLUSIONS+=$EXCLUSION
+      EXCLUSIONS+=("--exclude=$EXCLUSION")
     done
 
-    tar $EXCLUSIONS -zcvf "$INPUT_FILENAME" "$INPUT_PATH" $INPUT_CUSTOM || { printf "\n⛔ Unable to create %s archive.\n" "$INPUT_TYPE"; exit 1;  }
+    tar "${EXCLUSIONS[@]}" -zcvf "$INPUT_FILENAME" "${INPUT_PATH[@]}" "${INPUT_CUSTOM[@]}" || { printf "\n⛔ Unable to create %s archive.\n" "$INPUT_TYPE"; exit 1; }
   fi
 else
   printf "\n⛔ Invalid archiving tool.\n"; exit 1;
